4efd9bf37ddc0424a1bd4b6c5f467ca7248718a1
yeonji0 <kyj0279@gmail.com>
2022-07-21 18:19:55 +0900

    s

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -3,4 +3,23 @@ console.log(".index");
 
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0)
+    $(".overlay").show();
 })
+$(".overlay").click(function(){
+    $(".navbarClone").css("left","-100%");
+    $(".overlay").hide();
+});
+
+
+
+
+$(".bg").slideUp()
+$(".navbar>ul>li").hover(function(){
+    $(".bg,.submenuWrap").stop().slideDown()
+},function(){
+    $(".bg,.submenuWrap").stop().slideUp()
+
+})
+ 
+$(".navbar>ul>").clone().appendTo(".mMenu")
+
