6651b288a5c453523214ef61e0c3369f715c8b3a
yeonji0 <kyj0279@gmail.com>
2022-07-21 16:25:02 +0900

    s

--- 07company/test/js/script.js
+++ 07company/test/js/script.js
@@ -1,20 +1,18 @@
-console.log(".test");
+console.log("test");
 
 $(".navbar>ul>li").hover(function(){
     $(this).addClass("on");
 },function(){
     $(this).removeClass("on");
-
 });
 
 $(".toggle").click(function(){
-    $(".navbarClone").css("left",0)
+    $(".navbarClone").css("left",0);
     $(".overlay").show();
 })
-
 $(".overlay").click(function(){
-    $(".navbarClone").css("left","-100%")
+    $(".navbarClone").css("left","-100%");
     $(".overlay").hide();
-})
+});
 
-$(".navbar>ul").clone().appendTo.(".mMenu");
+$(".navbar>ul").clone().appendTo(".mMenu")
