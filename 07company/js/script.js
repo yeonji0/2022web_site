64199edaec5e2ca758039bd56705b4345bb725da
yeonji0 <kyj0279@gmail.com>
2022-07-28 17:54:30 +0900

    s

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -48,13 +48,28 @@ $(".modal").click(function(e){
     $(".overLayer img").attr("src",aModal)
 })
 
-$(".xicon").click(function(){
+$(".m_icon").click(function(){
     $(this).parent().parent().hide();
 })
 
 var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "fade",
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".ben-button-next",
+      prevEl: ".ben-button-prev",
     },
   });
+
+  setInterval(slideNum,3000);
+
+  let num = 0;
+  let view = 1;
+
+  function slideNum(){
+    num++;
+    if(num == $(".slide>div").length){
+        num = 0;
+    }
+    $(".slide>div").eq(num).css({"z-index":view++,opacity:0}).stop().animate({opacity:1},500)
+  }
