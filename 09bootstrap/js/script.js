9810a5b3455f4bfd075c3c5c14176b9a023074b9
yeonji0 <kyj0279@gmail.com>
2022-08-19 18:15:13 +0900

    d

---
+++ 09bootstrap/js/script.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper',{
+    loop:true,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    navigation:{
+        nextEl:"swiper-button-next",
+        prevEl:"swiper-button-prev"
+
+    }
+
+})
