6649dcddb2a8ee4ab6c47e78431a53a9c617a9fa
yeonji0 <kyj0279@gmail.com>
2022-09-23 16:41:05 +0900

    d

--- 10fullpage/js/script.js
+++ 10fullpage/js/script.js
@@ -4,3 +4,18 @@ $("#fullpage").fullpage({
     navigation:true,
     navigationPosition:"fp-right"
 });
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+});
