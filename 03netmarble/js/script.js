c2622c4faa043d4d55d8f776145d9e32fc2b69dc
yeonji0 <kyj0279@gmail.com>
2022-09-30 18:12:57 +0900

    d

---
+++ 03netmarble/js/script.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(){
+    let scrollY = window.pageYOffset;
+
+    if (scrollY > 10 ){
+        $(".navbar>ul>li>a").css("color","#333");
+        $("#header").css("background","#fff");
+        $(".logoWrap").css("display","block");
+
+    }else{
+        $(".navbar>ul>li>a").css("color","#fff");
+        $("#header").css("background","none");
+        $(".logoWrap").css("display","none");
+    }
+    $(".scroll").html(parseInt(scrollY))
+})
